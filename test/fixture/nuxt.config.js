90be1aa42c42f88b0b8cd40f579637039bbac068
Gaël Reyrol <gael@soyhuce.fr>
2018-09-07 10:02:00 +0200

    feat(module): Add rollbar error handling and client side error handling

--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -7,5 +7,10 @@ module.exports = {
   render: {
     resourceHints: false
   },
-  modules: ['@@']
+  modules: [
+    ['@@', {
+      serverAccessToken: 'ba78ddb653cd4f6fa17a0c171bdf4e8d',
+      clientAccessToken: 'fc61ebaa44784ec6870cc6c8b3b194b4'
+    }]
+  ]
 }
