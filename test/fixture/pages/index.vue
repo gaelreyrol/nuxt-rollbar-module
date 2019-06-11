940e98264803b52bf80a2b751609c4cc563038d1
RyoNkmr <ryonakamuraryo@gmail.com>
2019-06-12 07:41:24 +0900

    feat(module): Add server side error handling
    BREAKING CHANGE: change API `this.rollbar` into `this.$rollbar`

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -1,13 +1,16 @@
 <template>
-<div>
-  Works!
-</div>
+  <div>
+    Works!
+  </div>
 </template>
 
 <script>
 export default {
+  asyncData(ctx) {
+    ctx.$rollbar.debug("YoHo !");
+  },
   mounted() {
-    this.rollbar.debug('YoHo !')
+    this.$rollbar.debug("YoHo !");
   }
-}
+};
 </script>
