acf6c6b35367fd32bb3e755b36d7c295cf5b0cb5
RyoNkmr <ryonakamuraryo@gmail.com>
2019-06-12 11:04:55 +0900

    fix(module): inject RollbarAdapter to Vue.rollbar

--- test/fixture/pages/index.vue
+++ test/fixture/pages/index.vue
@@ -5,12 +5,15 @@
 </template>
 
 <script>
+import Vue from "vue";
+
 export default {
   asyncData(ctx) {
     ctx.$rollbar.debug("YoHo !");
   },
   mounted() {
     this.$rollbar.debug("YoHo !");
+    Vue.rollbar.debug("YoHo !");
   }
 };
 </script>
